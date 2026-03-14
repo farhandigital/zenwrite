22f19e94479439715853f67bc8c7f883385341ed
Invictus Navarchus <githubgpg.lumpiness807@passmail.com>
2026-03-15 04:37:57 +0700

    refactor: remove unused manual chunking
    1. lucide. Sveltkit override this and decided to merge with Svelte
    2. zip.js. We've no longer used it

--- vite.config.ts
+++ vite.config.ts
@@ -7,20 +7,12 @@ export default defineConfig({
 		rollupOptions: {
 			output: {
 				manualChunks(id) {
-					// Check for zip.js by its node_modules path
-					if (id.includes('node_modules/@zip.js')) {
-						return 'vendor-zip';
-					}
 					// Separate codemirror chunk
 					if (
 						id.includes('node_modules/@codemirror') ||
 						id.includes('node_modules/codemirror')
 					) {
 						return 'vendor-codemirror';
-					}
-					// Lucide icons in separate chunk
-					if (id.includes('node_modules/lucide-svelte')) {
-						return 'vendor-lucide';
 					}
 				},
 			},
