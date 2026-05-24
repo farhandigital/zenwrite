aace9cdfd0b72fdb2c56c801cb7758a60435782d
Invictus Navarchus <githubgpg.lumpiness807@passmail.com>
2026-05-24 07:33:59 +0700

    fix: setup portless integration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		port: 4200,
+		host: '127.0.0.1',
+		strictPort: true,
+	},
 	plugins: [sveltekit()],
 	build: {
 		rollupOptions: {
